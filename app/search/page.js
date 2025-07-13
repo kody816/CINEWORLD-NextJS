771aa0b58add15ce4898162af3a2d0b01a887fbb
kody816 <kodynott2@gmail.com>
2025-07-13 20:50:29 +0100

    Update Search page logic

--- app/search/page.js
+++ app/search/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import SearchDisplay from "@/components/display/SearchDisplay";
 import SearchBar from "@/components/searchbar/SearchBar";
 import SearchTitle from "@/components/title/SearchTitle";
@@ -9,46 +9,47 @@ const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
 const Search = () => {
   const [data, setData] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
   const [typedValue, setTypedValue] = useState("");
+  const [value] = useDebounce(typedValue, 500);
 
-  // Triggered on button click
   const handleSearch = (searchValue) => {
-    const getData = async () => {
-      if (searchValue !== "") {
-        const res = await fetch(
-          `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${searchValue}`
-        );
-        const result = await res.json();
-        setData(result.results);
-      }
-    };
-    getData();
+    if (!searchValue) return;
+    fetch(
+      `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${searchValue}`
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.results || []);
+      });
   };
 
-  // Triggered on typing
-  const handleTyping = (typedValue) => {
-    setTypedValue(typedValue);
+  const handleTyping = (value) => {
+    setTypedValue(value);
   };
-
-  const [value] = useDebounce(typedValue, 1000);
 
   useEffect(() => {
-    const getData = async () => {
-      if (typedValue !== "") {
-        const res = await fetch(
-          `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${value}`
-        );
-        const result = await res.json();
-        setData(result.results);
-      }
-    };
-    getData();
+    if (value) {
+      fetch(
+        `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${value}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setSuggestions(result.results || []);
+        });
+    } else {
+      setSuggestions([]);
+    }
   }, [value]);
 
   return (
     <div className="h-full">
       <SearchTitle />
-      <SearchBar onSearch={handleSearch} onTyping={handleTyping} />
+      <SearchBar
+        onSearch={handleSearch}
+        onTyping={handleTyping}
+        suggestions={suggestions}
+      />
       <SearchDisplay movies={data} />
     </div>
   );
