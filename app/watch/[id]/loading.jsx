9cd51bb1efd8602a4be4df81d46029249e41d057
kody816 <kodynott2@gmail.com>
2025-07-14 09:14:36 +0100

    Update loading.jsx

--- app/watch/[id]/loading.jsx
+++ app/watch/[id]/loading.jsx
@@ -1,3 +1,9 @@
+import React from "react";
+
 export default function Loading() {
-  return <div className="text-white text-center p-10">Loading player...</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-t from-[#141414] to-[#080808] text-yellow-400">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-yellow-400 border-solid"></div>
+    </div>
+  );
 }
