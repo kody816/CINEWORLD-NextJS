874055fcc91dbf4ff64d943ebf873cc18378dbd2
kody816 <kodynott2@gmail.com>
2025-07-14 13:21:52 +0100

    Update LayoutWrapper.jsx

--- components/layout/LayoutWrapper.jsx
+++ components/layout/LayoutWrapper.jsx
@@ -7,7 +7,7 @@ import BottomNav from "./BottomNav";
 export default function LayoutWrapper({ children }) {
   return (
     <>
-      {/* App Title for Mobile + Tablet only */}
+      {/* Mobile + iPad top banner */}
       <div className="md:hidden fixed top-0 left-0 w-full bg-black text-yellow-400 text-xl font-extrabold px-4 py-3 z-50 flex items-center gap-2 drop-shadow-[0_0_6px_rgba(255,255,0,0.5)]">
         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-yellow-400" viewBox="0 0 24 24">
           <path d="M8 5v14l11-7z" />
@@ -15,22 +15,22 @@ export default function LayoutWrapper({ children }) {
         Perez TV
       </div>
 
-      {/* Desktop Top Navigation */}
+      {/* Desktop Top Nav */}
       <div className="hidden lg:block">
         <TopNav />
       </div>
 
-      {/* Main Content with padding */}
-      <div className="pt-16 pb-20 lg:pt-0 lg:pb-0">
+      {/* Page Content with spacing to avoid overlap */}
+      <div className="pt-16 md:pt-16 pb-20 lg:pt-0 lg:pb-0">
         {children}
       </div>
 
-      {/* Mobile Bottom Nav for phones */}
+      {/* Mobile Nav (phones) */}
       <div className="md:hidden">
         <MobileNav />
       </div>
 
-      {/* Bottom Nav for tablets and desktops */}
+      {/* Bottom Nav for iPad/tablet */}
       <div className="hidden md:flex">
         <BottomNav />
       </div>
