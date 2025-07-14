d0109ee4904a69aab097205d28707c1ad188e6da
kody816 <kodynott2@gmail.com>
2025-07-14 20:58:33 +0100

    Update page.js

--- app/search/page.js
+++ app/search/page.js
@@ -1,10 +1,10 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
 import SearchDisplay from "@/components/display/SearchDisplay";
 import SearchBar from "@/components/searchbar/SearchBar";
 import SearchTitle from "@/components/title/SearchTitle";
-import React, { useEffect, useState } from "react";
-import { useDebounce } from "use-debounce";
 
 const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
@@ -41,11 +41,11 @@ const Search = () => {
   }, [value]);
 
   return (
-    <div className="min-h-[calc(100vh-56px)] px-4 pb-16 pt-4">
+    <main className="flex-1 px-4 pt-4 pb-24 md:pb-32 text-white">
       <SearchTitle />
       <SearchBar onSearch={handleSearch} onTyping={handleTyping} suggestions={suggestions} />
       <SearchDisplay movies={data} />
-    </div>
+    </main>
   );
 };
 
