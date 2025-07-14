106557ac8161cfa345f659175ba0efead82b06ca
kody816 <kodynott2@gmail.com>
2025-07-14 06:02:46 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,18 +11,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-black text-white">
-        {/* Mobile CineWorld Banner */}
-        <div className="lg:hidden fixed top-0 left-0 w-full bg-black text-yellow-400 text-xl font-bold px-4 py-3 z-50 shadow-md">
+        {/* CineWorld Banner visible on ALL screens */}
+        <div className="fixed top-0 left-0 w-full bg-black text-yellow-400 text-xl font-bold px-4 py-3 z-50 shadow-md text-center">
           CineWorld
         </div>
 
         {/* Desktop Nav */}
-        <div className="hidden lg:block">
+        <div className="hidden lg:block mt-14"> {/* Push nav down so it doesn't clash */}
           <TopNav />
         </div>
 
-        {/* Main Page Content with padding to avoid overlap */}
-        <div className="pt-16 pb-20 lg:pt-0 lg:pb-0">
+        {/* Main Page Content with top padding to prevent banner overlap */}
+        <div className="pt-20 pb-20 lg:pb-0">
           {children}
         </div>
 
