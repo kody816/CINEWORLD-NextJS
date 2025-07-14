b970e57b9739eda6daa13f92bb90b5928096380c
kody816 <kodynott2@gmail.com>
2025-07-14 13:30:43 +0100

    Update LayoutWrapper.jsx

--- components/layout/LayoutWrapper.jsx
+++ components/layout/LayoutWrapper.jsx
@@ -7,31 +7,31 @@ import BottomNav from "./BottomNav";
 export default function LayoutWrapper({ children }) {
   return (
     <>
-      {/* Mobile + iPad top banner */}
-      <div className="md:hidden fixed top-0 left-0 w-full bg-black text-yellow-400 text-xl font-extrabold px-4 py-3 z-50 flex items-center gap-2 drop-shadow-[0_0_6px_rgba(255,255,0,0.5)]">
+      {/* 📺 Perez TV only on mobile & tablet */}
+      <div className="lg:hidden fixed top-0 left-0 w-full bg-black text-yellow-400 text-xl font-extrabold px-4 py-3 z-50 flex items-center gap-2 drop-shadow-[0_0_6px_rgba(255,255,0,0.5)]">
         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-yellow-400" viewBox="0 0 24 24">
           <path d="M8 5v14l11-7z" />
         </svg>
         Perez TV
       </div>
 
-      {/* Desktop Top Nav */}
+      {/* Top Nav on desktop only */}
       <div className="hidden lg:block">
         <TopNav />
       </div>
 
-      {/* Page Content with spacing to avoid overlap */}
-      <div className="pt-16 md:pt-16 pb-20 lg:pt-0 lg:pb-0">
+      {/* Main content (offset for header/footer) */}
+      <div className="pt-16 pb-20 lg:pt-0 lg:pb-0">
         {children}
       </div>
 
-      {/* Mobile Nav (phones) */}
+      {/* Mobile nav (phones only) */}
       <div className="md:hidden">
         <MobileNav />
       </div>
 
-      {/* Bottom Nav for iPad/tablet */}
-      <div className="hidden md:flex">
+      {/* Tablet nav (iPad etc) */}
+      <div className="hidden md:flex lg:hidden">
         <BottomNav />
       </div>
     </>
