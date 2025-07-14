6e23702474c72e3781c5176c0b9f6262d5910589
kody816 <kodynott2@gmail.com>
2025-07-14 01:31:27 +0100

    Updated Homepage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
-import HorizontalSection from "@/components/display/HorizontalSection";
 import HeroBanner from "@/components/display/HeroBanner";
+import HorizontalSection from "@/components/display/HorizontalSection";
+import ContinueWatching from "@/components/display/ContinueWatching";
 import Title from "@/components/title/Title";
 
 async function getSection(url) {
@@ -8,7 +9,7 @@ async function getSection(url) {
     `https://api.themoviedb.org/3${url}?api_key=${apiKey}&language=en-US`
   );
   const data = await res.json();
-  return data.results;
+  return data.results?.slice(0, 10) || []; // limit each section to 10 items
 }
 
 export default async function Home() {
@@ -18,26 +19,49 @@ export default async function Home() {
     getSection("/movie/now_playing"),
   ]);
 
-  const featured = trendingMovies[0]; // pick first movie
+  const featured = trendingMovies[0];
 
   return (
-    <div className="h-auto">
+    <div className="h-auto w-full">
+      {/* Hero Banner */}
       <HeroBanner movie={featured} />
-      <Title />
-      <HorizontalSection
-        title="Trending Movies"
-        movies={trendingMovies}
-        link="/trending/movie"
-      />
-      <HorizontalSection
-        title="Trending Series"
-        movies={trendingSeries}
-        link="/trending/tv"
-      />
-      <HorizontalSection
-        title="New Releases"
-        movies={newReleases}
-      />
+
+      {/* Continue Watching */}
+      <div className="px-4 sm:px-6 lg:px-10 mt-6">
+        <ContinueWatching />
+      </div>
+
+      {/* Homepage Title */}
+      <div className="px-4 sm:px-6 lg:px-10">
+        <Title />
+      </div>
+
+      {/* Trending Movies */}
+      <div className="px-4 sm:px-6 lg:px-10">
+        <HorizontalSection
+          title="Trending Movies"
+          movies={trendingMovies}
+          link="/trending/movie"
+        />
+      </div>
+
+      {/* Trending Series */}
+      <div className="px-4 sm:px-6 lg:px-10">
+        <HorizontalSection
+          title="Trending Series"
+          movies={trendingSeries}
+          link="/trending/tv"
+        />
+      </div>
+
+      {/* New Releases */}
+      <div className="px-4 sm:px-6 lg:px-10 pb-8">
+        <HorizontalSection
+          title="New Releases"
+          movies={newReleases}
+          link="/new-releases"
+        />
+      </div>
     </div>
   );
 }
